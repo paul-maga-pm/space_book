4fbfcd9e42882f29414d7dc0ba22b54dfaf9ee40
paul_maga_pm <paul.maga.pm@gmail.com>
2021-11-15 22:41:33 +0200

    Refactored database repository classes

--- src/main/java/socialnetwork/repository/database/UserDatabaseRepository.java
+++ src/main/java/socialnetwork/repository/database/UserDatabaseRepository.java
@@ -5,17 +5,11 @@ import socialnetwork.exceptions.DatabaseException;
 import socialnetwork.repository.RepositoryInterface;
 
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Repository for User model implementation on network database
  */
-public class UserDatabaseRepository implements RepositoryInterface<Long, User> {
-    private String url;
-    private String user;
-    private String password;
+public class UserDatabaseRepository extends AbstractDatabaseRepository<Long, User> {
 
     /**
      * Creates a new database with the given connection data
@@ -23,97 +17,19 @@ public class UserDatabaseRepository implements RepositoryInterface<Long, User> {
      * @param user user of the server
      * @param password master password of the server
      */
-    public UserDatabaseRepository(String url, String user, String password){
-        this.url = url;
-        this.user = user;
-        this.password = password;
+    public UserDatabaseRepository(String url, String user, String password) {
+        super(url, user, password);
     }
 
-    @Override
-    public Optional<User> save(User user) {
-        try(Connection connection = DriverManager.getConnection(url, this.user, password)) {
-            PreparedStatement findSql = createFindUserByIdStatement(user.getId(), connection);
-            ResultSet resultSet = findSql.executeQuery();
-            if(resultSet.next())
-                return Optional.of(createUserFromResultSet(resultSet));
-            else{
-                PreparedStatement insertSql = createInsertStatementForUser(user, connection);
-                insertSql.executeUpdate();
-                return Optional.empty();
-            }
-        } catch (SQLException exception) {
-            throw new DatabaseException(exception.getMessage());
-        }
-    }
-
-    @Override
-    public List<User> getAll() {
-        try(Connection connection = DriverManager.getConnection(url, user, password)) {
-            List<User> users = new ArrayList<>();
-            PreparedStatement selectSql = connection.prepareStatement("SELECT * FROM users");
-            ResultSet resultSet = selectSql.executeQuery();
-            while(resultSet.next())
-                users.add(createUserFromResultSet(resultSet));
-            return users;
-        } catch (SQLException exception) {
-            throw new DatabaseException(exception.getMessage());
-        }
-    }
-
-    @Override
-    public Optional<User> findById(Long id) {
-        try(Connection connection = DriverManager.getConnection(url, user, password)) {
-            PreparedStatement findSql = createFindUserByIdStatement(id, connection);
-            ResultSet resultSet = findSql.executeQuery();
-            if(resultSet.next())
-                return Optional.of(createUserFromResultSet(resultSet));
-            return Optional.empty();
-        } catch (SQLException exception) {
-            throw new DatabaseException(exception.getMessage());
-        }
-    }
-
-    @Override
-    public Optional<User> update(User newValue) {
-        try(Connection connection = DriverManager.getConnection(url, user, password)) {
-            PreparedStatement findSql = createFindUserByIdStatement(newValue.getId(), connection);
-            ResultSet resultSet = findSql.executeQuery();
-            if(resultSet.next()){
-                User oldValue = createUserFromResultSet(resultSet);
-                PreparedStatement updateSql = createUpdateStatementForUser(newValue, connection);
-                updateSql.executeUpdate();
-                return Optional.of(oldValue);
-            }
-            return Optional.empty();
-        } catch (SQLException exception) {
-            throw new DatabaseException(exception.getMessage());
-        }
-    }
-
-    @Override
-    public Optional<User> remove(Long id) {
-        try(Connection connection = DriverManager.getConnection(url, user, password)) {
-            PreparedStatement findSql = createFindUserByIdStatement(id, connection);
-            ResultSet resultSet = findSql.executeQuery();
-            if(resultSet.next()){
-                User oldValue = createUserFromResultSet(resultSet);
-                PreparedStatement updateSql = createDeleteStatementForUser(id, connection);
-                updateSql.executeUpdate();
-                return Optional.of(oldValue);
-            }
-            return Optional.empty();
-        } catch (SQLException exception) {
-            throw new DatabaseException(exception.getMessage());
-        }
-    }
 
     /**
-     * Creates a query for id, firstName and lastName of a user
+     * Creates a query for selecting id, firstName and lastName of a user
      * @param id identifier of user that will be selected
      * @param connection Connection to database
-     * @return a PreparedStatement object representing the query for selecting the friendship from database
+     * @return a PreparedStatement object representing the query for selecting the user from database
      */
-    private PreparedStatement createFindUserByIdStatement(Long id, Connection connection){
+    @Override
+    public PreparedStatement createFindStatementForEntityWithId(Connection connection, Long id){
         try{
             String findSqlString = "SELECT id, first_name, last_name FROM users WHERE id = ?";
             PreparedStatement findSql = connection.prepareStatement(findSqlString);
@@ -125,12 +41,28 @@ public class UserDatabaseRepository implements RepositoryInterface<Long, User> {
     }
 
     /**
+     * Creates a query that selects all entries from the users relation
+     * @param connection Connection to database
+     * @return a Preparedstatement object representing the query for selecting all users from the database
+     */
+    @Override
+    public PreparedStatement createSelectAllStatement(Connection connection) {
+        try{
+            String selectAllStatementString = "SELECT * FROM users";
+            return connection.prepareStatement(selectAllStatementString);
+        } catch (SQLException exception) {
+            throw new DatabaseException(exception.getMessage());
+        }
+    }
+
+    /**
      * Creates a PreparedStatement that removes the user with the given id
      * @param id identifier of the user that will be removed
      * @param connection Connection object to a database
      * @return PreparedStatement representing a query that will remove the user with the given id
      */
-    private PreparedStatement createDeleteStatementForUser(Long id, Connection connection){
+    @Override
+    public PreparedStatement createDeleteStatementForEntityWithId(Connection connection, Long id){
         try{
             String deleteSqlStr = "DELETE FROM users WHERE id=?";
             PreparedStatement updateSql = connection.prepareStatement(deleteSqlStr);
@@ -148,7 +80,8 @@ public class UserDatabaseRepository implements RepositoryInterface<Long, User> {
      * @param connection Connection object to a database
      * @return PreparedStatement representing a query that will update the user with the same id as newValue
      */
-    private PreparedStatement createUpdateStatementForUser(User newValue, Connection connection){
+    @Override
+    public PreparedStatement createUpdateStatementForEntity(Connection connection, User newValue){
         try{
             String updateSqlStr = "UPDATE users SET first_name=?, last_name=? WHERE id=?";
             PreparedStatement updateSql = connection.prepareStatement(updateSqlStr);
@@ -167,7 +100,8 @@ public class UserDatabaseRepository implements RepositoryInterface<Long, User> {
      * @param connection Connection object to a database
      * @return a PreparedStatement that represent a query that will add the user into the database
      */
-    private PreparedStatement createInsertStatementForUser(User user, Connection connection){
+    @Override
+    public PreparedStatement createInsertStatementForEntity(Connection connection, User user){
         try{
             String insertSqlStr = "INSERT INTO users(id, first_name, last_name) values (?,?,?)";
             PreparedStatement insertSql = connection.prepareStatement(insertSqlStr);
@@ -185,7 +119,7 @@ public class UserDatabaseRepository implements RepositoryInterface<Long, User> {
      * @param resultSet contains id, firstName, lastName of the user and must point to a valid row
      * @return User with the given data
      */
-    private User createUserFromResultSet(ResultSet resultSet){
+    public User createEntityFromResultSet(ResultSet resultSet){
         try{
             Long id = resultSet.getLong("id");
             String firstName = resultSet.getString("first_name");
@@ -195,4 +129,6 @@ public class UserDatabaseRepository implements RepositoryInterface<Long, User> {
             throw new DatabaseException(exception.getMessage());
         }
     }
+
+
 }
