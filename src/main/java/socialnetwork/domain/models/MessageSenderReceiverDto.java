99a1a2f6e9139734e1ede522d514cd8b59407878
Daria Manu <daria.manu@gmail.com>
2021-11-17 21:14:15 +0200

    Added MessageSenderreceiverdto

---
+++ src/main/java/socialnetwork/domain/models/MessageSenderReceiverDto.java
@@ -0,0 +1,26 @@
+package socialnetwork.domain.models;
+
+import socialnetwork.utils.containers.MessageSenderReceiverDtoId;
+
+import java.util.Objects;
+
+public class MessageSenderReceiverDto extends Entity<MessageSenderReceiverDtoId>{
+
+    public MessageSenderReceiverDto(Long messageId, Long senderId, Long receiverId) {
+        super(new MessageSenderReceiverDtoId(messageId, senderId, receiverId));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MessageSenderReceiverDto)) return false;
+        if (!super.equals(o)) return false;
+        MessageSenderReceiverDto that = (MessageSenderReceiverDto) o;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode());
+    }
+}
