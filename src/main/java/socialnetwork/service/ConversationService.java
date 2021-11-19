f0c61084492417f45035c391ef346f19bce1226a
Daria Manu <daria.manu@gmail.com>
2021-11-19 18:20:29 +0200

    Added ConversationService class

---
+++ src/main/java/socialnetwork/service/ConversationService.java
@@ -0,0 +1,22 @@
+package socialnetwork.service;
+
+import socialnetwork.domain.models.*;
+import socialnetwork.domain.validators.EntityValidatorInterface;
+import socialnetwork.repository.RepositoryInterface;
+
+public class ConversationService {
+    private RepositoryInterface<Long, MessageDto> messageDtoRepository;
+    private RepositoryInterface<MessageSenderReceiverDtoId, MessageSenderReceiverDto> messageSenderReceiverDtoRepository;
+    private RepositoryInterface<Long, ReplyDto> replyDtoRepository;
+    private EntityValidatorInterface<Long, Message> messageValidator;
+
+    public ConversationService(RepositoryInterface<Long, MessageDto> messageDtoRepository,
+                               RepositoryInterface<MessageSenderReceiverDtoId, MessageSenderReceiverDto> messageSenderReceiverDtoRepository,
+                               RepositoryInterface<Long, ReplyDto> replyDtoRepository,
+                               EntityValidatorInterface<Long, Message> messageValidator) {
+        this.messageDtoRepository = messageDtoRepository;
+        this.messageSenderReceiverDtoRepository = messageSenderReceiverDtoRepository;
+        this.replyDtoRepository = replyDtoRepository;
+        this.messageValidator = messageValidator;
+    }
+}
