f47d8163aaef987cc2cf78dc8cd8c521e83eed03
paul_maga_pm <paul.maga.pm@gmail.com>
2021-12-24 22:35:21 +0200

    fixed notification date view bug

--- src/main/java/socialnetwork/models/RejectedFriendRequestModel.java
+++ src/main/java/socialnetwork/models/RejectedFriendRequestModel.java
@@ -18,6 +18,6 @@ public class RejectedFriendRequestModel extends NotificationModel {
     public Node getViewForModel() {
         return new Label(super.toString() + " You declined friend request sent by " +
                 dto.getSender() + " on " +
-                dto.getFriendRequest().getDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd hh:mm")));
+                dto.getFriendRequest().getDate().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
     }
 }
